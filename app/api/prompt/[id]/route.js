65d186dd4df4bd7cd096911943bf2be4e0d9c19a
harshajavvaji <102543049+harshajavvaji@users.noreply.github.com>
2024-11-30 22:00:41 +0530

    Resolved search issue

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -43,7 +43,7 @@ export const PATCH = async ( request, {params}) => {
 export const DELETE = async ( request, {params}) => {
     try{
         await Prompt.findByIdAndRemove(params.id);
-        return new Response("Prompt deleted sucessfully", {status: 200})
+        return new Response("Prompt     d sucessfully", {status: 200})
     } catch(error) {
         return new Response("Failed to delete Prompt", {status: 500})
     }
